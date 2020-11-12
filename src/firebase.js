72822a4694b55632e2eedaf98e2fb34446ac04e9
Imad Elmahrad <70642895+ImadEM21@users.noreply.github.com>
2020-11-12 23:12:22 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,16 @@ import 'firebase/firestore';
 
 
 const config = {
-    apiKey: "AIzaSyApJxLPTt8KuYdErgbV_yqOy0cPY7_vAZw",
-    authDomain: "crm-react-b12d3.firebaseapp.com",
-    databaseURL: "https://crm-react-b12d3.firebaseio.com",
-    projectId: "crm-react-b12d3",
-    storageBucket: "crm-react-b12d3.appspot.com",
-    messagingSenderId: "236838375779",
-    appId: "1:236838375779:web:ad7f1a3894d92ce4530356",
-    measurementId: "G-ERVZSTD0BV"
+    apiKey: "Your api key",
+    authDomain: "Your auth domain",
+    databaseURL: "Your Url's database",
+    projectId: "Your project's id",
+    storageBucket: "Storage Bucket",
+    messagingSenderId: "MessagingSenderId",
+    appId: "App's ID",
+    measurementId: "Measurement ID"
 };
 
 firebase.initializeApp(config);
 
-export default firebase;
+export default firebase;            
